969cf833a368c40a5237ad44b792f3e535f2c1a0
mengshang918 <mengshang918@gmail.com>
2021-12-15 16:15:50 +0800

    Revert "feat(drip-form-plugin-eggjs): add drip-form-plugin-eggjs (#20)"
    This reverts commit cef14a9ab551f29cf863b4ac62cd52d6e2281231.

--- packages/drip-form-plugin-eggjs/test/fixtures/apps/drip-validator-test/app/router.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.exports = (app) => {
-  const { router, controller } = app
-
-  router.post('/', controller.home.index)
-}
--- packages/drip-form-plugin-eggjs/test/fixtures/apps/drip-validator-test/config/config.default.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-exports.keys = '123456'
-
-exports.security = {
-  ctoken: false,
-  csrf: false,
-}
