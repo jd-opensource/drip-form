a23b523b764388aa02c05cff4399803837fae92b
mengshang918 <mengshang918@gmail.com>
2022-01-20 11:38:06 +0800

    feat(generator): 对象容器支持配置更多样式
    是否带边框、销毁折叠隐藏的面板、图标位置、是否可折叠、折叠面板透明且无边框、会否展示箭头

--- packages/generator/src/fields/container/object.field.ts
+++ packages/generator/src/fields/container/object.field.ts
@@ -37,8 +37,78 @@ const config: Field = {
         fieldKey: 'active',
         type: 'boolean',
         title: '是否默认展开',
+        default: false,
         ui: {
           type: 'switch',
+          vcontrol: 'return props.formData.ui.mode==="collapse"',
+        },
+      },
+      {
+        fieldKey: 'bordered',
+        type: 'boolean',
+        title: '是否带边框',
+        default: true,
+        ui: {
+          type: 'switch',
+          vcontrol: 'return props.formData.ui.mode==="collapse"',
+        },
+      },
+      {
+        fieldKey: 'destroyInactivePanel',
+        type: 'boolean',
+        title: '销毁折叠隐藏的面板',
+        default: false,
+        ui: {
+          type: 'switch',
+          vcontrol: 'return props.formData.ui.mode==="collapse"',
+        },
+      },
+      {
+        fieldKey: 'expandIconPosition',
+        type: 'string',
+        title: '图标位置',
+        default: 'left',
+        ui: {
+          type: 'radio',
+          options: [
+            { label: '左', value: 'left' },
+            { label: '右', value: 'right' },
+          ],
+          vcontrol: 'return props.formData.ui.mode==="collapse"',
+        },
+      },
+      {
+        fieldKey: 'collapsible',
+        type: 'string',
+        title: '是否可折叠',
+        default: 'header',
+        ui: {
+          type: 'radio',
+          options: [
+            { label: '可折叠', value: 'header' },
+            { label: '不可折叠', value: 'disabled' },
+          ],
+          vcontrol: 'return props.formData.ui.mode==="collapse"',
+        },
+      },
+      {
+        fieldKey: 'ghost',
+        type: 'boolean',
+        title: '折叠面板透明无边框',
+        default: false,
+        ui: {
+          type: 'switch',
+          vcontrol: 'return props.formData.ui.mode==="collapse"',
+        },
+      },
+      {
+        fieldKey: 'showArrow',
+        type: 'boolean',
+        title: '是否展示箭头',
+        default: true,
+        ui: {
+          type: 'switch',
+          vcontrol: 'return props.formData.ui.mode==="collapse"',
         },
       },
     ],
