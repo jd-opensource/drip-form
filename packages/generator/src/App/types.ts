36f76768dac9a484f9324bb09f90a9166ab6ed76
mengshang918 <mengshang918@gmail.com>
2022-02-24 15:12:38 +0800

    feat(generator): viewport区域操作栏支持配置显隐

--- packages/generator/src/App/types.ts
+++ packages/generator/src/App/types.ts
@@ -1,7 +1,11 @@
 import type { UnitedSchema } from '@jdfed/utils'
 import type { UiComponents } from '@jdfed/drip-form'
 import type { Field } from '@generator/fields/types'
-import type { HeaderConfig, ComponentsData } from '@generator/store'
+import type {
+  HeaderConfig,
+  ComponentsData,
+  ViewportConfig,
+} from '@generator/store'
 
 export type GeneratorType = {
   // 由外部导入的联合schema，用于初始化
@@ -10,11 +14,13 @@ export type GeneratorType = {
   theme?: UiComponents
   // 左侧拖拽区域展示控制（若配置了components,该选项无效）
   customComponents?: Array<Field>
-  // 组件自定义（自定义左侧组件区域、viewport区域、属性配置、校验配置）
+  // 组件自定义（自定义左侧组件区域、viewport区域、编辑区域（属性配置、校验配置））
   components?: ComponentsData
   headerConfig?: HeaderConfig
+  viewportConfig?: ViewportConfig
   /**
-   * 不推荐，后续版本会合并以下api；推荐使用headerConfig代替
+   * 以下API均不推荐 START
+   * 后续版本会合并以下api；推荐使用headerConfig代替
    */
   // 导出的文案 (推荐使用headerConfig.exportText)
   exportText?: string
