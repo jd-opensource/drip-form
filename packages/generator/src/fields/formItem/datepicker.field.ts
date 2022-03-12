a238424c1aed9382735d1e2de1ec60890dd851c4
mengshang918 <mengshang918@gmail.com>
2022-03-12 11:45:04 +0800

    feat(generator): datepicker add default configuration

--- packages/generator/src/fields/formItem/datepicker.field.ts
+++ packages/generator/src/fields/formItem/datepicker.field.ts
@@ -1,4 +1,90 @@
 import { Field } from '../types'
+import type { OnChange } from '@jdfed/utils'
+
+const formatList = {
+  date: 'YYYY-MM-DD HH:mm:ss',
+  week: 'YYYY-wo',
+  month: 'YYYY-MM',
+  quarter: 'YYYY-quarter',
+  year: 'YYYY',
+}
+
+const changeShowTime: OnChange = ({ dispatch, getKey, val }) => {
+  dispatch({
+    type: 'setUi',
+    action: {
+      set: {
+        [getKey('ui.default', 'uiSchema') + '.showTime']: val,
+        [getKey('ui.default', 'uiSchema') + '.format']: val
+          ? 'YYYY-MM-DD HH:mm:ss'
+          : 'YYYY-MM-DD',
+        [getKey('ui.default__range.0', 'uiSchema') + '.showTime']: val,
+        [getKey('ui.default__range.1', 'uiSchema') + '.showTime']: val,
+        [getKey('ui.default__range.0', 'uiSchema') + '.format']: val
+          ? 'YYYY-MM-DD HH:mm:ss'
+          : 'YYYY-MM-DD',
+        [getKey('ui.default__range.1', 'uiSchema') + '.format']: val
+          ? 'YYYY-MM-DD HH:mm:ss'
+          : 'YYYY-MM-DD',
+      },
+    },
+  })
+  dispatch({
+    type: 'setValidate',
+    action: {
+      set: {
+        [getKey('ui.default', 'dataSchema') + '.default']: '',
+      },
+    },
+  })
+  dispatch({
+    type: 'setData',
+    action: {
+      set: {
+        'ui.default': '',
+        'ui.default__range': [],
+        'ui.format': val ? 'YYYY-MM-DD HH:mm:ss' : 'YYYY-MM-DD',
+      },
+    },
+  })
+}
+
+const changePicker: OnChange = ({ dispatch, getKey, val }) => {
+  dispatch({
+    type: 'setUi',
+    action: {
+      set: {
+        [getKey('ui.default', 'uiSchema') + '.picker']: val,
+        [getKey('ui.default', 'uiSchema') + '.format']:
+          formatList[val as keyof typeof formatList],
+        [getKey('ui.default__range.0', 'uiSchema') + '.picker']: val,
+        [getKey('ui.default__range.1', 'uiSchema') + '.picker']: val,
+        [getKey('ui.default__range.0', 'uiSchema') + '.format']:
+          formatList[val as keyof typeof formatList],
+        [getKey('ui.default__range.1', 'uiSchema') + '.format']:
+          formatList[val as keyof typeof formatList],
+      },
+    },
+  })
+  dispatch({
+    type: 'setValidate',
+    action: {
+      set: {
+        [getKey('ui.default', 'dataSchema') + '.default']: '',
+      },
+    },
+  })
+  dispatch({
+    type: 'setData',
+    action: {
+      set: {
+        'ui.default': '',
+        'ui.default__range': [],
+        'ui.format': formatList[val as keyof typeof formatList],
+      },
+    },
+  })
+}
 
 /**
  * 日期选择器
@@ -9,9 +95,9 @@ const config: Field = {
   unitedSchema: {
     type: 'string',
     title: '日期选择框',
-    format: 'date-time',
     ui: {
       type: 'datePicker',
+      showTime: true,
     },
   },
   propertyConfig: {
@@ -42,7 +128,12 @@ const config: Field = {
         type: 'boolean',
         title: '是否开启时间选择功能',
         default: true,
-        ui: { type: 'switch' },
+        ui: {
+          type: 'switch',
+          onChange: changeShowTime,
+          // eslint-disable-next-line quotes
+          vcontrol: "return props.formData.ui.picker==='date'",
+        },
       },
       {
         fieldKey: 'picker',
@@ -52,6 +143,7 @@ const config: Field = {
         ui: {
           type: 'select',
           style: { width: '50%' },
+          onChange: changePicker,
           options: [
             { label: '日', value: 'date' },
             { label: '周', value: 'week' },
@@ -76,10 +168,44 @@ const config: Field = {
         type: 'string',
         title: '默认值',
         ui: {
-          type: 'text',
-          placeholder: '2021-11-11 11:11:11',
+          type: 'datePicker',
+          picker: 'date',
+          showTime: true,
           vcontrol: 'return !props.formData.ui.range',
         },
+      },
+      {
+        fieldKey: 'default__range',
+        type: 'array',
+        title: '默认值',
+        default: [],
+        ui: {
+          type: 'array',
+          mode: 'tuple',
+          vcontrol: 'return props.formData.ui.range',
+        },
+        maxItems: 2,
+        minItems: 2,
+        items: [
+          {
+            type: 'string',
+            title: '起始时间',
+            ui: {
+              type: 'datePicker',
+              picker: 'date',
+              showTime: true,
+            },
+          },
+          {
+            type: 'string',
+            title: '终止时间',
+            ui: {
+              type: 'datePicker',
+              picker: 'date',
+              showTime: true,
+            },
+          },
+        ],
       },
       {
         fieldKey: 'placeholder__range',
@@ -108,37 +234,6 @@ const config: Field = {
             ui: {
               type: 'text',
               placeholder: '终止时间占位符',
-            },
-          },
-        ],
-      },
-      {
-        fieldKey: 'default__range',
-        type: 'array',
-        title: '默认值',
-        default: [],
-        ui: {
-          type: 'array',
-          mode: 'tuple',
-          vcontrol: 'return props.formData.ui.range',
-        },
-        maxItems: 2,
-        minItems: 2,
-        items: [
-          {
-            type: 'string',
-            title: '起始时间',
-            ui: {
-              type: 'text',
-              placeholder: '2021-11-11 11:11:11',
-            },
-          },
-          {
-            type: 'string',
-            title: '终止时间',
-            ui: {
-              type: 'text',
-              placeholder: '2021-12-12 12:12:12',
             },
           },
         ],
