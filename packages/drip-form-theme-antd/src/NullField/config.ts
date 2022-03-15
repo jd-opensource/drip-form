dca0687a89200278cdd4018dbc49f683a4ec0906
mengshang918 <mengshang918@gmail.com>
2022-03-15 18:30:25 +0800

    feat: nullField add styleType configuration (#158)

--- packages/drip-form-theme-antd/src/NullField/config.ts
+++ packages/drip-form-theme-antd/src/NullField/config.ts
@@ -21,6 +21,74 @@ const config = {
         default: '这是一段文字',
         ui: { type: 'text' },
       },
+      {
+        type: 'string',
+        title: '类型',
+        default: 'none',
+        ui: {
+          type: 'radio',
+          options: [
+            {
+              label: '无',
+              value: 'none',
+            },
+            {
+              label: '成功',
+              value: 'success',
+            },
+            {
+              label: '失败',
+              value: 'error',
+            },
+            {
+              value: 'warning',
+              label: '警告',
+            },
+            {
+              label: '提示',
+              value: 'info',
+            },
+          ],
+        },
+        fieldKey: 'styleType',
+      },
+      {
+        type: 'object',
+        title: '对象容器',
+        ui: {
+          type: 'object',
+          mode: 'normal',
+          showTitle: false,
+        },
+        fieldKey: 'style',
+        default: {},
+        schema: [
+          {
+            type: 'string',
+            title: '背景颜色',
+            default: '#ffffff',
+            ui: {
+              type: 'colorPicker',
+              defaultValue: '#ffffff',
+              vcontrol:
+                'return !props.formData.ui.styleType||props.formData.ui.styleType==="none"',
+            },
+            fieldKey: 'backgroundColor',
+          },
+          {
+            type: 'string',
+            title: '文本颜色',
+            default: '#000000',
+            ui: {
+              type: 'colorPicker',
+              defaultValue: '#000000',
+              vcontrol:
+                'return !props.formData.ui.styleType||props.formData.ui.styleType==="none"',
+            },
+            fieldKey: 'color',
+          },
+        ],
+      },
     ],
   },
 }
--- packages/drip-form-theme-antd/src/NullField/index.tsx
+++ packages/drip-form-theme-antd/src/NullField/index.tsx
@@ -2,17 +2,55 @@
  * @Author: jiangxiaowei
  * @Date: 2020-05-14 13:33:14
  * @Last Modified by: jiangxiaowei
- * @Last Modified time: 2022-03-13 12:29:08
+ * @Last Modified time: 2022-03-15 14:49:37
  */
 import React, { memo } from 'react'
 
 export type NullFieldProps = {
   style: React.CSSProperties
   text: string
+  styleType: 'none' | 'success' | 'error' | 'warning' | 'info'
 }
 
-const NullField = ({ style, text }: NullFieldProps) => {
-  return <div style={{ wordBreak: 'break-all', ...style }}>{text}</div>
+const commonStyle = {
+  height: 40,
+  lineHeight: '40px',
+  padding: '0 16px',
+  borderRadius: '2px',
+}
+
+const styleTypeMap = {
+  none: null,
+  success: {
+    backgroundColor: '#F6FFED',
+    // color: '#feb00e',
+    ...commonStyle,
+  },
+  error: {
+    backgroundColor: '#FFF2F0',
+    // color: '#feb00e',
+    ...commonStyle,
+  },
+  warning: {
+    backgroundColor: '#fff7e1',
+    color: '#feb00e',
+    ...commonStyle,
+  },
+  info: {
+    backgroundColor: '#E6F7FF',
+    // color: '#feb00e',
+    ...commonStyle,
+  },
+}
+
+const NullField = ({ style, text, styleType = 'none' }: NullFieldProps) => {
+  return (
+    <div
+      style={{ wordBreak: 'break-all', ...style, ...styleTypeMap[styleType] }}
+    >
+      {text}
+    </div>
+  )
 }
 
 export const nullField = memo(NullField)
