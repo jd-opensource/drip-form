97eddeb3f49d3b348338fdf0389bedc786a7bdb7
mengshang918 <mengshang918@gmail.com>
2022-03-19 13:33:01 +0800

    feat(generator): support customize layout width

--- packages/generator/src/fields/common/propertyConfig/layout.schema.ts
+++ packages/generator/src/fields/common/propertyConfig/layout.schema.ts
@@ -21,15 +21,7 @@ const unitedSchema = {
       type: 'string',
       title: '组件宽度',
       ui: {
-        type: 'slider',
-        showNumber: false,
-        min: 0,
-        max: 100,
-        marks: {
-          0: { label: '0' },
-          50: { label: '50%' },
-          100: { label: '100%' },
-        },
+        type: 'text',
       },
     },
     {
