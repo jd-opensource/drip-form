24fb7b1d31322f69bceaef3b4e4a886f77f4c694
mengshang918 <mengshang918@gmail.com>
2022-05-09 11:12:27 +0800

    fix(generator): fixed dragging and sorting child elements in tuple containers

--- packages/generator/src/hooks/useAddField.tsx
+++ packages/generator/src/hooks/useAddField.tsx
@@ -3,7 +3,7 @@
  * @Author: jiangxiaowei
  * @Date: 2021-10-08 10:20:13
  * @Last Modified by: jiangxiaowei
- * @Last Modified time: 2022-04-27 18:02:10
+ * @Last Modified time: 2022-05-09 10:14:55
  */
 import { useCallback, useContext } from 'react'
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
@@ -42,22 +42,22 @@ const useAddField = (): AddField => {
     ({
       unitedSchema,
       fieldKey = selectedKey || '',
-      closestEdge = 'bottom',
+      closestEdge,
       oldFieldKey,
     }) => {
       if (!canEdit) {
         message.warning('请先保存JSON编辑结果，再退出！', 2)
         return
       }
+      const newClosestEdge = closestEdge || 'bottom'
       // 生成当前新增表单项的key
       let newFieldKey = fieldKeyFn(unitedSchema)
 
       // 选中新增的表单项
       let selectKey = newFieldKey
       const keyPath = fieldKey.split('.')
-      const lastKey = keyPath.length > 1 ? fieldKey.split('.').pop() : ''
       const parentKey =
-        closestEdge === 'over'
+        newClosestEdge === 'over'
           ? fieldKey
           : keyPath.slice(0, keyPath.length - 1).join('.')
       let type =
@@ -74,7 +74,7 @@ const useAddField = (): AddField => {
           // vieport区域拖拽
           if (oldFieldKey) {
             const lastFieldKey = oldFieldKey.split('.').pop() as string
-            if (closestEdge != 'over') {
+            if (newClosestEdge != 'over') {
               const keyPath = oldFieldKey.split('.')
               const oldParentKey = keyPath
                 .slice(0, keyPath.length - 1)
@@ -99,27 +99,58 @@ const useAddField = (): AddField => {
           selectKey = `${parentKey ? `${parentKey}.` : ''}${newFieldKey}`
           break
         case 'array':
-          if (closestEdge != 'over') {
+          if (newClosestEdge != 'over') {
             // viewport区域拖拽
             if (oldFieldKey) {
               newFieldKey = oldFieldKey.split('.').pop() as string
             }
           }
           selectKey = `${parentKey}${
-            closestEdge != 'over' ? `.0.${newFieldKey}` : '.0'
+            newClosestEdge != 'over' ? `.0.${newFieldKey}` : '.0'
           }`
           break
         case 'tuple': {
-          const curKey = lastKey ? (lastKey as string) : fieldKey
-          // 当前插入位置
-          const index =
-            closestEdge === 'over'
-              ? 0
-              : ['left', 'top'].includes(closestEdge)
-              ? +curKey
-              : +curKey + 1
-          newFieldKey = `${index}`
-          selectKey = `${parentKey}.${index}`
+          const curKey = fieldKey.split('.').pop() as string
+          if (newClosestEdge != 'over') {
+            if (oldFieldKey) {
+              const keyPath = oldFieldKey.split('.')
+              const oldParentKey = keyPath
+                .slice(0, keyPath.length - 1)
+                .join('.')
+              if (oldParentKey === parentKey) {
+                shouldDelete = false
+                newFieldKey = oldFieldKey.split('.').pop() as string
+                let direction: 'topToBottom' | 'bottomToTop'
+                if (+curKey > +newFieldKey) {
+                  direction = 'topToBottom'
+                } else {
+                  direction = 'bottomToTop'
+                }
+                selectKey = `${parentKey}.${
+                  direction === 'topToBottom'
+                    ? ['top', 'left'].includes(newClosestEdge)
+                      ? +curKey - 1
+                      : curKey
+                    : curKey
+                }`
+              } else {
+                newFieldKey = String(
+                  ['left', 'top'].includes(newClosestEdge)
+                    ? +curKey
+                    : +curKey + 1
+                )
+                selectKey = `${parentKey}.${newFieldKey}`
+              }
+            } else {
+              newFieldKey = String(
+                ['left', 'top'].includes(newClosestEdge) ? +curKey : +curKey + 1
+              )
+              selectKey = `${parentKey}.${newFieldKey}`
+            }
+          } else {
+            newFieldKey = '0'
+            selectKey = `${parentKey}.${newFieldKey}`
+          }
           break
         }
         default:
@@ -127,18 +158,22 @@ const useAddField = (): AddField => {
       }
       generatorContext.current?.__generator__.addField({
         fieldKey: newFieldKey,
-        closestEdge,
+        closestEdge: newClosestEdge,
         unitedSchema,
         overFieldKey:
-          !selectedKey && addFieldLocation === 'bottom' && closestEdge != 'over'
+          // 未选中任何表单，左侧点击新增，并且设置方向为bottom时，自动添加到最后一个元素后
+          !selectedKey && addFieldLocation === 'bottom' && !closestEdge
             ? lastFieldKey
             : fieldKey,
         shouldDelete,
         cb: () => {
+          // 设置选中的表单
           setSelected(selectKey)
+          // 设置选中的表单类型
           setCurType(unitedSchema.ui.type)
         },
       })
+      // // viewport区域拖拽且非排序模式，需要删除原来位置的表单
       if (shouldDelete && oldFieldKey) {
         deleteField(oldFieldKey)
       }
